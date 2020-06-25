c6bb48276a084a33322916e6ac60e04c370835f0
Andreas Vija <andreas.vija@stacc.ee>
2020-06-25 14:29:48 +0300

    minor changes

--- qtlmap_prep/promoter_annotations_to_gtf.py
+++ qtlmap_prep/promoter_annotations_to_gtf.py
@@ -1,22 +1,17 @@
 """
 tss_id
-promoter_rank
 gene_name
-gene_count
 chr
 peak_start
 peak_end
-peak_score
 strand
-tss_start
-tss_end
 ->
 seqname - chr
 source - fantom5
 feature - promotor
 start - peak_start
 end - peak_end
-score - peak_score
+score - .
 strand - strand
 frame - .
 attribute:
@@ -32,23 +27,21 @@ with open(inputfilename, 'r') as infile:
 
 		infile.readline()
 		for inline in infile:
-			#print(inline)
-			
+
 			incols = inline.strip().split('\t')
 			outline = ''
-			
-			outline += incols[4] + '\t' # seqname - chr
+
+			outline += incols[2] + '\t' # seqname - chr
 			outline += 'fantom5' + '\t' # source - fantom5
 			outline += 'promotor' + '\t' # feature - promotor
-			outline += incols[5] + '\t' # start - peak_start
-			outline += incols[6] + '\t' # end - peak_end
-			outline += incols[7] + '\t' # score - peak_score
-			outline += incols[8] + '\t' # strand - strand
+			outline += incols[3] + '\t' # start - peak_start
+			outline += incols[4] + '\t' # end - peak_end
+			outline += '+' + '\t' # score - .
+			outline += incols[5] + '\t' # strand - strand
 			outline += '.' + '\t' # frame - .
-			
+
 			outline += 'gene_id' + ' "' + incols[0] + '";' # promotor_id tss_id; // feature
-			outline += 'geeni_id' + ' "' + incols[2] + '";\t' # gene_id gene_name; // meta-feature
+			outline += 'geeni_id' + ' "' + incols[1] + '";\t' # gene_id gene_name; // meta-feature
 			outline += '\n'
-			
-			#print(outline)
+
 			outfile.write(outline)
