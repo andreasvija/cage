16a1b50d5f78b8fd44118ff3dffc0bfb0f9ce9f2
Andreas Vija <andreasvija@hotmail.com>
2020-07-10 17:17:31 +0300

    we need a pipeline

--- analysis/annots.sh
+++ analysis/annots.sh
@@ -3,34 +3,37 @@
 #SBATCH -p main
 #SBATCH -J new_annots
 #SBATCH -t 23:00:00
-#SBATCH -c 4
+#SBATCH -c 8
 #SBATCH --mem=16G
 
 module load R/3.6.1
 module load python-3.6.0
+module load squashfs/4.3-lt2t
+module load singularity/3.5.3
 
-Rscript annots.R
+#Rscript annots.R
 
-git clone git@github.com:andreasvija/txrevise.git
+#rm -rf txrevise
+#git clone git@github.com:andreasvija/txrevise.git
 cd txrevise
-git checkout CAGE
+#git checkout CAGE
 
-cp ../new_transcripts_25.rds data/
+#cp ../new_transcripts_25.rds data/
 cd scripts
 mkdir processed
-wget -O processed/Homo_sapiens.GRCh38.96.gtf.gz ftp://ftp.ensembl.org/pub/release-96/gtf/homo_sapiens/Homo_sapiens.GRCh38.96.gtf.gz
+#wget -O processed/Homo_sapiens.GRCh38.96.gtf.gz ftp://ftp.ensembl.org/pub/release-96/gtf/homo_sapiens/Homo_sapiens.GRCh38.96.gtf.gz
 
-conda activate
 annotation="Homo_sapiens.GRCh38.96"
-python extractTranscriptTags.py --gtf processed/"$annotation".gtf.gz > processed/"$annotation".transcript_tags.txt
-conda deactivate
+#python extractTranscriptTags.py --gtf processed/"$annotation".gtf.gz > processed/"$annotation".transcript_tags.txt
 
 Rscript prepareAnnotations.R --gtf processed/"$annotation".gtf.gz --tags processed/"$annotation".transcript_tags.txt --out processed/"$annotation".txrevise_annotations.rds
 
+#singularity build txrevise.img docker://kauralasoo/txrevise:latest
 cd ..
-Rscript data/prepare_CAGE_data.R
+singularity exec -B /gpfs/:/gpfs/ scripts/txrevise.img Rscript data/prepare_CAGE_data.R
 cd scripts
 
 rm -rf SlurmOut
 mkdir SlurmOut
-snakemake -p processed/Homo_sapiens.GRCh38.96_log.txt --cluster ./snakemake_submit_UT.py --jobs 100 --config fill=TRUE start_end_diff=25 cage=../data/CAGE_promoter_annotations_25.rds --use-singularity
+#snakemake -p processed/Homo_sapiens.GRCh38.96_log.txt --cluster ./snakemake_submit_UT.py --jobs 100 --config fill=TRUE start_end_diff=25 cage=../data/CAGE_promoter_annotations_25.rds --use-singularity
+#rm txrevise.img
--- analysis/visualization.sh
+++ analysis/visualization.sh
@@ -2,9 +2,10 @@
 
 #SBATCH -p main
 #SBATCH -J run_visualizations
-#SBATCH -t 23:00:00
-#SBATCH -c 4
+#SBATCH -t 2-00:00:00
+#SBATCH -c 8
 #SBATCH --mem=16G
 
 module load R/3.6.1
+mkdir plots
 Rscript visualization.R
