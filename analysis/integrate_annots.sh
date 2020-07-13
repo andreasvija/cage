447cf194926fcec96991e9b5e3783b4c46258bef
Andreas Vija <andreas.vija@stacc.ee>
2020-07-13 12:28:05 +0300

    changed snakemake

--- analysis/integrate_annots.sh
+++ analysis/integrate_annots.sh
@@ -3,12 +3,11 @@
 #SBATCH -p main
 #SBATCH -J integrate_annots
 #SBATCH -t 23:00:00
-#SBATCH -c 8
-#SBATCH --mem=16G
+#SBATCH -c 1
+#SBATCH --mem=4G
 
-module load python-3.6.0
-module load squashfs/4.3-lt2t
-module load singularity/3.5.3
+#module load squashfs/4.3-lt2t
+#module load singularity/3.5.3
 
 git clone git@github.com:andreasvija/txrevise.git
 cd txrevise
@@ -21,6 +20,7 @@ mkdir processed
 wget -O processed/Homo_sapiens.GRCh38.96.gtf.gz ftp://ftp.ensembl.org/pub/release-96/gtf/homo_sapiens/Homo_sapiens.GRCh38.96.gtf.gz
 
 mkdir SlurmOut
+source ~/.bashrc
 conda activate snakemake
-snakemake -s cage.Snakefile -p processed/Homo_sapiens.GRCh38.96_log.txt --cluster ./snakemake_submit_UT.py --jobs 100 --config fill=TRUE start_end_diff=25 --use-singularity
+snakemake -s cage.Snakefile --cluster ./snakemake_submit_UT.py --jobs 100 --config fill=TRUE start_end_diff=25 annotation=Homo_sapiens.GRCh38.96 --use-singularity
 conda deactivate
