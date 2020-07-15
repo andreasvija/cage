72ca5aaa798c1d2392ee91fb05586054027a1052
Andreas Vija <andreasvija@hotmail.com>
2020-07-15 10:16:26 +0300

    pipeline works

--- analysis/integrate_annots.sh
+++ analysis/integrate_annots.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-#SBATCH -p amd
+#SBATCH -p main
 #SBATCH -J integrate_annots
 #SBATCH -t 23:00:00
 #SBATCH -c 1
 #SBATCH --mem=4G
 
-#module load squashfs/4.3-lt2t
-#module load singularity/3.5.3
+module load squashfs/4.3-lt2t
+module load singularity/3.5.3
 
 git clone git@github.com:andreasvija/txrevise.git
 cd txrevise
 git checkout CAGE
 git pull
 
-cp ../new_transcripts_25.rds data/
 cd scripts
 mkdir processed
+cp ../../new_transcripts_25.rds processed/
 wget -O processed/Homo_sapiens.GRCh38.96.gtf.gz ftp://ftp.ensembl.org/pub/release-96/gtf/homo_sapiens/Homo_sapiens.GRCh38.96.gtf.gz
+
+if [ ! -f txrevise.img ]; then
+    singularity build txrevise.img docker://kauralasoo/txrevise:latest
+fi
 
 mkdir SlurmOut
 source ~/.bashrc
