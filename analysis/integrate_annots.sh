e14b6738a4235c476e76fe64c2e5e946deba480b
Andreas Vija <andreasvija@hotmail.com>
2020-07-24 15:48:34 +0300

    small fixes

--- analysis/integrate_annots.sh
+++ analysis/integrate_annots.sh
@@ -20,11 +20,13 @@ git checkout CAGE
 git pull
 
 cd scripts
+mkdir processed
+mkdir processed/input
 cp ../../new_transcripts_25.rds processed/input/
 wget -O processed/input/${ANNOTATION}.gtf.gz ftp://ftp.ensembl.org/pub/release-${RELEASE_N}/gtf/homo_sapiens/${ANNOTATION}.gtf.gz
 
-if [ ! -f input/txrevise.img ]; then
-    singularity build input/txrevise.img docker://kauralasoo/txrevise:latest
+if [ ! -f txrevise.img ]; then
+    singularity build txrevise.img docker://kauralasoo/txrevise:latest
 fi
 
 mkdir SlurmOut
@@ -32,5 +34,5 @@ source ~/.bashrc
 conda activate snakemake
 OUTPUT=processed/${ANNOTATION}_all_completed.txt
 rm $OUTPUT
-snakemake -ps cage.Snakefile $OUTPUT --cluster ./snakemake_submit_UT.py --jobs 200 --config fill=TRUE Ns='25' --use-singularity
+snakemake -pns cage.Snakefile $OUTPUT --cluster ./snakemake_submit_UT.py --jobs 200 --config fill=TRUE Ns="25" --use-singularity
 conda deactivate
