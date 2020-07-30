aeee3030a82d4879fa82c43ad27b6ba7f2e39fa5
Andreas Vija <andreas.vija@stacc.ee>
2020-07-30 13:54:22 +0300

    try intersect

--- qtlmap_prep/filter_common_variants.sh
+++ qtlmap_prep/filter_common_variants.sh
@@ -3,10 +3,9 @@
 #SBATCH -p main
 #SBATCH -J filter_common_variants
 #SBATCH -t 08:00:00
-#SBATCH -c 4
+#SBATCH -c 8
 #SBATCH --mem=8G
 
 module load bcftools-1.9
 
-bcftools query -f '%ID' -o variants /gpfs/hpc/projects/GENCORD/Garieri_2017/Garieri_filtered.no_DS.vcf.gz
-bcftools query -f '%ID' -o variants_geuvadis /gpfs/hpc/projects/genomic_references/GEUVADIS/genotypes/GEUVADIS_GRCh38_filtered.vcf.gz
+bcftools isec -n~11 -w1 cage_variants_common.vcf.gz -w2 txrevise_variants_common.vcf.gz /gpfs/hpc/projects/GENCORD/Garieri_2017/Garieri_filtered.no_DS.vcf.gz /gpfs/hpc/projects/genomic_references/GEUVADIS/genotypes/GEUVADIS_GRCh38_filtered.vcf.gz
