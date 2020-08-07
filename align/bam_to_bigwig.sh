b08cf2c685bdc4c399c6865b5049e650ac28cc26
Andreas Vija <andreasvija@hotmail.com>
2020-08-07 14:39:28 +0300

    no multimapping results

--- align/bam_to_bigwig.sh
+++ align/bam_to_bigwig.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # screen, ssh stage1
 
-#SBATCH -p main
+#SBATCH -p amd
 #SBATCH -J bam_to_bigwig
 #SBATCH -t 24:00:00
-#SBATCH -c 8
-#SBATCH --mem=32G
+#SBATCH -c 2
+#SBATCH --mem=4G
 
 module load python-3.6.0
 mkdir SlurmOut
-snakemake -s bam_to_bigwig.Snakefile --cluster ./snakemake_submit_UT.py -p out_bigwig.txt --configfile config.yaml --jobs 8
+snakemake -s bam_to_bigwig.Snakefile --cluster ./snakemake_submit_UT.py -p out_bigwig.txt --configfile config.yaml --jobs 20
--- align/bwa.sh
+++ align/bwa.sh
@@ -4,9 +4,9 @@
 #SBATCH -p main
 #SBATCH -J bwa
 #SBATCH -t 24:00:00
-#SBATCH -c 8
-#SBATCH --mem=32G
+#SBATCH -c 2
+#SBATCH --mem=4G
 
 module load python-3.6.0
 mkdir SlurmOut
-snakemake -s bwa.Snakefile --cluster ./snakemake_submit_UT.py -p out.txt --configfile config.yaml --jobs 8
+snakemake -s bwa.Snakefile --cluster ./snakemake_submit_UT.py -p out.txt --configfile config.yaml --jobs 20
