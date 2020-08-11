bd11a6f484346f2f0a036993b6a23537ca178fb3
Andreas Vija <andreas.vija@stacc.ee>
2020-08-11 19:28:09 +0300

    other n pipelines

---
+++ pipeline/annots_200.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#screen, ssh stage1
+
+#SBATCH -p main
+#SBATCH -J annots_pipeline_200
+#SBATCH -t 24:00:00
+#SBATCH -c 8
+#SBATCH --mem=24G
+
+module load java-1.8.0_40
+module load singularity
+module load nextflow
+
+mkdir annots_200
+cd annots_200
+
+git clone https://github.com/eQTL-Catalogue/rnaseq
+git clone https://github.com/eQTL-Catalogue/qcnorm
+git clone https://github.com/eQTL-Catalogue/qtlmap.git
+
+NXF_VER=18.10.1 nextflow \
+	run rnaseq/main.nf \
+	-profile eqtl_catalogue \
+	--readPathsFile /gpfs/hpc/projects/eQTLCatalogue/SampleArcheology/readPaths/GEUVADIS_readPaths.tsv \
+	--unstranded \
+	--skip_qc \
+	--skip_multiqc \
+	--skip_stringtie \
+	--saveAlignedIntermediates \
+	--run_txrevise \
+	--txrevise_gffs '../../analysis/txrevise/scripts/processed/Homo_sapiens.GRCh38.96_CAGE-200/txrevise_*.gff3' \
+	-resume \
+	-executor.queueSize 100
+
+nextflow \
+	run qcnorm/normalisation.nf \
+	-profile tartu_hpc \
+	-resume \
+	--study_name txrevise_annots \
+	--quant_results_path results \
+	--sample_meta_path ../../analysis/GEUVADIS_EUR.tsv \
+	--txrev_pheno_meta_path ../../analysis/txrevise/scripts/processed/Homo_sapiens.GRCh38.96_CAGE-200/txrevise_CAGE-200_phenotype_metadata.tsv.gz \
+	--skip_exon_norm \
+	--skip_tx_norm \
+	--skip_leafcutter_norm \
+	--outdir qcnorm_out
+
+../../nextflow/nextflow \
+	run qtlmap/main.nf \
+	-profile tartu_hpc \
+	--studyFile ../sources_annots_200.tsv \
+	--cis_window 200000 \
+	--run_permutation true \
+	--is_imputed FALSE \
+	--varid_rsid_map_file /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/dbSNP_b151_GRCh38p7_splitted_var_rsid.vcf.gz \
+	-resume
+
+gunzip results/sumstats/txrevise_annots.permuted.txt.gz -c > ../../analysis/txrev_annots_200.permuted.txt
